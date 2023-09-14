85e67c5ddafbb4012d87d9e363cee10187c39eb1
Elinora Melgarejo <jacksonfugate@ufl.edu>
2023-09-14 07:20:32 -0400

    Swerve Modules Base Code

---
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -0,0 +1,24 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public final double L = 46;
+public final double W = 46;
+
+public void drive (double x1, double y1, double x2) {
+    double r = Math.sqrt ((L * L) + (W * W));
+    y1 *= -1;
+
+    double a = x1 - x2 * (L/r);
+    double b = x1 + x2 * (L/r);
+    double c = y1 - x2 * (W/r);
+    double d = y1 + x2 * (W/r);
+
+    double backRightSpeed    = Math.sqrt ((a * a) + (d * d));
+    double backLeftSpeed     = Math.sqrt ((a * a) + (c * c));
+    double frontRightSpeed   = Math.sqrt ((b * b) + (d * d));
+    double frontLeftSpeed    = Math.sqrt ((b * b) + (c * c));
+
+}
+
