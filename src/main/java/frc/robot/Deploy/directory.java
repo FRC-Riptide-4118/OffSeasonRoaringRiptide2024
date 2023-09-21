ad0c7402336c369bc847f268e651cabbe840dd28
Elinora Melgarejo <jacksonfugate@ufl.edu>
2023-09-21 16:18:06 -0400

    JSON Files

--- src/main/java/frc/robot/Deploy/directory.java
+++
@@ -1,7 +0,0 @@
-import java.io.File;
-import edu.wpi.first.wpilibj.Filesystem;
-import swervelib.parser.SwerveParser;
-import swervelib.SwerveDrive;
-
-File swerveJsonDirectory=new File(Filesystem.getDeployDirectory(),"swerve");
-SwerveDrive swerveDrive=new SwerveParser(swerveJsonDirectory).createSwerveDrive();
